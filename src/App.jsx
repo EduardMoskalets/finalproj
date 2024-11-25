dd246a565848ab69bdd9386a82cceb025e5484d3
Eduard <edik.moskalets@gmail.com>
2024-11-25 22:23:27 +0200

    upd

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.module.css';
 import SettingsButtons from './components/DailyInfo/SettingButtons';
 import UserBar from './components/UserBar/UserBar';
+import UserLogo from './components/ChooseDate/Userlogo';
 
 function App() {
   return (
     <div>
+
+      <UserLogo/>
       <UserBar
         name="Nadia"
         avatarUrl="https://example.com/avatar.jpg" //  ссылка на аватар пользователя
