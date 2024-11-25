19de9ba4ba0df3b354d5b7e87a1af01400e938b4
Eduard <edik.moskalets@gmail.com>
2024-11-25 11:40:27 +0200

    userbar

--- src/components/UserBar/UserBar.jsx
+++ src/components/UserBar/UserBar.jsx
@@ -1,5 +1,9 @@
 import css from "./UserBar.module.css";
 import { useState, useRef } from 'react';
+import { TbSettings } from "react-icons/tb";
+import { MdLogout } from "react-icons/md";
+import { BsChevronUp } from "react-icons/bs";
+
 
 
 
@@ -15,15 +19,22 @@ const UserBar = ({ name, avatarUrl }) => {
     <div className={css.user_button_container}>
             <button className={css.user_button} onClick={togglePopover} ref={buttonRef}>
                 <span className={css.username}>{name}</span>
-                <img src={avatarUrl} alt="#" className={css.avatar} />
+              <img src={avatarUrl} alt="#" className={css.avatar} />
+              <BsChevronUp className={css.iconArrow} />
                 
       </button>
 
       { /* Всплывающее окно (popover) */ }
       {showPopover && (
-        <div className={css.popover}>
-            <button type="button" className={css.inPopover}><span className={css.buttonSetting}>Setting</span></button>
-            <button type="button" className={css.inPopover}><span className={css.buttonLogOut}>Log out</span></button>
+              <div className={css.popover}>
+                  <button type="button" className={css.inPopover}>
+                    <div className={css.icon_setting}><TbSettings  /></div>
+                    <span className={css.buttonSetting}>Setting</span>
+                  </button>
+                  <button type="button" className={css.inPopoverLogOut}>
+                    <div className={css.icon_logout}><MdLogout /></div>
+                    <span className={css.buttonLogOut}>Log out</span>
+                  </button>
         </div>
       )}
     </div>
