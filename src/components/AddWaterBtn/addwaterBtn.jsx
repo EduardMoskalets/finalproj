ff73ecc4dcbdc98d2fae0484fa9f12035363fb16
Eduard <edik.moskalets@gmail.com>
2024-12-01 18:16:03 +0200

    upd

---
+++ src/components/AddWaterBtn/addwaterBtn.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const addwaterBtn = () => {
+  return (
+      <div>
+          {/* <p>{user?.name || user?.email}</p> */}
+      </div>
+  )
+}
+
+export default addwaterBtn
